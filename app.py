f3ad0917b81e44bfe188b6aef80e1cdf2b20da76
A.A Suvorov <saneksking@gmail.com>
2024-05-20 19:22:18 +0700

    Переделал программу, теперь для вывода репозиториев используется библиотека githubinformer

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import requests
 import json
+from githubinformer.github_informer import GitHubInformer
 
 
 def main():
     print('Программа для вывода всех открытых репозиториев разработчиков на Github!')
     print(''.center(86, '-'))
     user = input('Введите никнейм разработчика: ')
-    api_url = f'https://api.github.com/users/{user}/repos'
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        repositories = response.json()
-        for repo in repositories:
-            print(f'{repo["full_name"]}: {repo["html_url"]}')
+    github_informer = GitHubInformer(user=user)
+    info = github_informer.get_all_info()
+    if not info:
+        print('Такого пользователя несуществует!')
     else:
-        print(f'Ошибка! Код ответа {response.status_code}')
+        choice = input('Выберете действие:\n'
+                       'a: Вывести все репозитории;\n'
+                       'c: Вывести колличество открытых репозиториев у разработчика.\n'
+                       ': ')
+        if choice == 'a' or choice == 'A':
+            for repo in info:
+                print(f'{repo["full_name"]}: {repo["html_url"]}')
+        elif choice == 'c' or choice == 'C':
+            print(f'Колличество открытых репозиториев у разработчика {user}:'
+                  f' {github_informer.get_repositories_count()}')
+        else:
+            print('Вы ввели неверное значение!')
     print('Программа завершена!')
 
 
