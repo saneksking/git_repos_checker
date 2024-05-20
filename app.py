38a78a680c1fb360c87568d94864f56a08034a75
A.A Suvorov <saneksking@gmail.com>
2024-05-20 19:52:50 +0700

    Временный коммит

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-import requests
-import json
+import shutil
+
 from githubinformer.github_informer import GitHubInformer
 
 
+def term_width():
+    return shutil.get_terminal_size()[0]
+
+
 def main():
-    print('Программа для вывода всех открытых репозиториев разработчиков на Github!')
-    print(''.center(86, '-'))
+    print(''.center(term_width(), '*'))
+    print('Программа для вывода всех открытых репозиториев разработчиков на Github!'.center(term_width()))
+    print(''.center(term_width(), '*'))
     user = input('Введите никнейм разработчика: ')
     github_informer = GitHubInformer(user=user)
     info = github_informer.get_all_info()
     if not info:
-        print('Такого пользователя несуществует!')
+        print(''.center(term_width(), '-'))
+        print('Репозитории данного пользователя не найдены!')
     else:
         choice = input('Выберете действие:\n'
                        'a: Вывести все репозитории;\n'
                        'c: Вывести колличество открытых репозиториев у разработчика.\n'
                        ': ')
-        if choice == 'a' or choice == 'A':
-            for repo in info:
-                print(f'{repo["full_name"]}: {repo["html_url"]}')
+        if choice in ['a', 'A', 'ф', 'Ф']:
+            for n, repo in enumerate(info, 1):
+                print(f'{n}) {repo["full_name"]}: {repo["html_url"]}')
         elif choice == 'c' or choice == 'C':
             print(f'Колличество открытых репозиториев у разработчика {user}:'
                   f' {github_informer.get_repositories_count()}')
         else:
             print('Вы ввели неверное значение!')
+    print(''.center(term_width(), '='))
     print('Программа завершена!')
 
 
